fd4ad5f3cd338a43da069ca2ab47ce31abf4c0c9
olivi <olivi@CWELPC>
2025-10-15 01:37:37 +0200

    Dodaj pliki projektów.

---
+++ Python/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+int main()
+{
+    sf::RenderWindow gameWindow(sf::VideoMode({ 1400, 650 }), "Python The Game", sf::Style::Titlebar | sf::Style::Close);
+
+    while (gameWindow.isOpen())
+    {
+        
+
+
+
+    }
+    
+}
